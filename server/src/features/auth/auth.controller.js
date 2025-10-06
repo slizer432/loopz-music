ba708ea85a52ef1add50b3e8eddb636ddd7bd1ca
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2025-10-06 21:00:22 +0700

    add register

--- server/src/features/auth/auth.controller.js
+++ server/src/features/auth/auth.controller.js
@@ -36,3 +36,31 @@ export const login = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const emailExists = await User.findOne({ email });
+    const usernameExists = await User.findOne({ username });
+    if (usernameExists) {
+      return res.status(400).json({ error: "Username already taken" });
+    }
+    if (emailExists) {
+      return res.status(400).json({ error: "Email already registered" });
+    }
+    const user = await User.create({ username, email, password });
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
